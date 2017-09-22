73567973314d43251d8b3a7b83cf6b174756cd3b
Kyle Eisenbarger <keisenb@ksu.edu>
2017-09-22 00:08:39 -0500

    Finished post image and get image, mocked trigger methods

--- server/app.py
+++ server/app.py
@@ -11,11 +11,10 @@ with open("config.yml", 'r') as ymlfile:
 UPLOAD_FOLDER = './images'
 ALLOWED_EXTENSIONS = set(['jpg', 'jpeg'])
 
-app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-
 mysql = MySQL()
 
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['MYSQL_DATABASE_USER'] = cfg['mysql']['user']
 app.config['MYSQL_DATABASE_PASSWORD'] = cfg['mysql']['password']
 app.config['MYSQL_DATABASE_DB'] = cfg['mysql']['db']
@@ -25,45 +24,70 @@ mysql.init_app(app)
 
 
 
+# GET /api/gui
 @app.route('/api/gui')
 def api_gui():
-
     try:
         conn = mysql.connect()
         cursor = conn.cursor()
         cursor.execute("SELECT * FROM images WHERE processed = 0 LIMIT 1")
         data = cursor.fetchone()
         if data is None:
-            return "No new images to process"
+            return "", 204
         if data[1]:
             cursor.execute("UPDATE images SET processed = 1 WHERE id = %s", data[0])
             conn.commit()
             return send_from_directory(directory='images', filename=data[1])
-        return "unable to retrieve image", 400
+        return "Image name was invalid", 400
     except:
-        return "unable to retrieve image", 400
+        return "Exception when retrieving image, check logs", 400
 
 
+# POST /api/image
 @app.route('/api/image', methods = { 'POST' })
 def api_image():
-    if 'image' not in request.files:
-        return 'No image was included in request', 400
-    file = request.files['image']
-    if file.filename == '':
-        return 'File name was null', 400
-    if file and allowed_file(file.filename):
-        filename = secure_filename(file.filename)
-        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-        #todo save filename to db
-        conn = mysql.connect()
-        cursor = conn.cursor()
-        cursor.execute("INSERT INTO images (image_name) values (%s) ", filename)
-        data = cursor.fetchall()
-        if len(data) is 0:
-            conn.commit()
-            return 'Successfully uploaded the image'
-        return 'Unable to save image in database', 400
-    return 'Issue saving image'
+    try:
+        if 'image' not in request.files:
+            return 'No image was included in request', 400
+        file = request.files['image']
+        if file.filename == '':
+            return 'File name was invalid', 400
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            conn = mysql.connect()
+            cursor = conn.cursor()
+            cursor.execute("INSERT INTO images (image_name) values (%s) ", filename)
+            data = cursor.fetchall()
+            if len(data) is 0:
+                conn.commit()
+                return 'Successfully uploaded the image'
+            return 'Unable to save image in database', 400
+        return 'File is invalid or invalid file type', 400
+    except:
+        return 'Exception while saving image', 400
+
+
+
+# PUT /api/trigger/start
+@app.route('/api/trigger/start', methods = { 'PUT' })
+def api_trigger_start():
+    try:
+        return 'start'
+
+    except:
+        return 'Exception caught when attempting request', 400
+
+
+
+# PUT /api/trigger/stop
+@app.route('/api/trigger/stop', methods = { 'PUT' })
+def api_trigger_stop():
+    try:
+        return 'stop'
+
+    except:
+        return 'Exception caught when attempting request', 400
 
 
 
@@ -73,7 +97,6 @@ def api_image():
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
-
 
 
 if __name__ == '__main__':
