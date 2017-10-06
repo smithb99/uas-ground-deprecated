4f4722628aa99df934c0f2cec4969dec4710cc99
Kyle Eisenbarger <keisenb@ksu.edu>
2017-10-05 19:02:50 -0500

    Minor changes

--- server/app.py
+++ server/app.py
@@ -67,22 +67,26 @@ def api_post_processed_image():
         data = request.form
         if(data is None):
             return 'Request was empty', 400
-        if(data['confirmed'] is None or data['shape'] is None or data['shape_color'] is None or data['letter'] is None or data['letter_color'] is None or data['orientation'] is None):
-            return 'Missing parameter. Required parameters are: confirmed, shape, shape_color, letter, letter_color, orientation'
         f = request.files['image']
         if f.filename == '':
             return 'File name was invalid', 400
         if f and allowed_file(f.filename):
             filename = secure_filename(f.filename)
             f.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            image = Image(image_name=filename, processed=False)
+            image = Image(image_name=filename, processed=True, confirmed=data['confirmed'], shape=data['shape'], 
+                            shape_color=data['shape_color'], letter=data['letter'], letter_color=data['letter_color'], 
+                            orientation=data['orientation'])
             db.session.add(image)
             db.session.commit()
+            #todo: submit image for judging
             return 'Successfully uploaded the image'
         return 'File is invalid or invalid file type', 400
     except SQLAlchemyError as error:
         print(error)
         return 'Exception while saving image', 400
+    except HTTPException as error:
+        print(error)
+        return 'Missing paramter', 400
     except Exception as error:
         print(error)
         return "Unknown issue. Check logs", 400
