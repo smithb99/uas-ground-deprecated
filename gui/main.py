f6cc40c86a158446d22359e80a6079151616b83b
smithb99 <bhsmith1999@gmail.com>
2017-10-10 23:59:51 -0500

    Added background images, fullscreen config, and exiting
    Signed-off-by: Braedon Smith <bhsmith1999@gmail.com>

--- gui/main.py
+++ gui/main.py
@@ -1,10 +1,30 @@
 import configparser
 import os
 import pathlib
+import random
 import tkinter
+
+from PIL import Image, ImageTk
 
 __version__ = "2.0.0"
 __config__ = None
+
+root = None
+
+
+def stop():
+    root.destroy()
+
+
+def read_config(section, key):
+    return __config__.get(section, key)
+
+
+def resize_image(image, width):
+    width_percent = (width / float(image.size[0]))
+    h_size = int((float(image.size[1]) * float(width_percent)))
+
+    return image.resize((width, h_size), Image.ANTIALIAS)
 
 
 def write_config(section, key, value):
@@ -12,17 +32,36 @@ def write_config(section, key, value):
 
 
 def main():
+    mode = __config__.getboolean("GUI", "Fullscreen")
+    width = read_config("GUI", "WindowWidth")
+    height = read_config("GUI", "WindowHeight")
+
+    global root
     root = tkinter.Tk()
+
     root.title("Kansas State University UAS Client")
 
-    button_frame = tkinter.Frame(root)
+    image = Image.open("assets/ksu" + str(random.randint(1, 2)) + ".png")
 
-    server_button = tkinter.Button(root, text="Connect to Server")
-    exit_button = tkinter.Button(root, text="Exit")
+    if mode:
+        root.attributes("-fullscreen", True)
+        image = resize_image(image, root.winfo_screenwidth())
+    else:
+        root.attributes("-fullscreen", False)
+        image = resize_image(image, width)
 
-    button_frame.pack()
-    server_button.pack()
-    exit_button.pack()
+    photo = ImageTk.PhotoImage(image)
+
+    label = tkinter.Label(root, width=width, height=height, image=photo,
+                          anchor=tkinter.NW)
+
+    label.pack(fill=tkinter.BOTH, expand=tkinter.YES)
+
+    server_button = tkinter.Button(label, text="Connect to Server")
+    exit_button = tkinter.Button(label, text="Exit", command=stop)
+
+    server_button.grid(row=0)
+    exit_button.grid(row=1)
 
     root.mainloop()
 
@@ -47,15 +86,14 @@ if __name__ == "__main__":
             "Version": __version__,
             "WindowWidth": "",
             "WindowHeight": "",
-            "Mode": "WINDOWED"
+            "Fullscreen": "true"
         }
 
         with open(config_path, "w") as config_file:
             config.write(config_file)
             config_file.close()
     else:
-        __config__ = configparser.ConfigParser(allow_no_value=True).read(
-            config_path
-        )
+        __config__ = configparser.ConfigParser(allow_no_value=True)
+        __config__.read(config_path)
 
     main()
